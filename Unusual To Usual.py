f592c3cb14d38e11f92313700fc189c11aac2b0e
ALIZAIDIDAGOAT <alizaidi3c@gmail.com>
2026-04-02 18:08:34 +0500

    Unusual To Usual

---
+++ Unusual To Usual.py
@@ -0,0 +1,13 @@
+x = "coNgrAtuLaTioNS"
+print(str.lower(x))
+
+print(str.upper(x))
+
+print ("Enter The First String :")
+strOne = input()
+
+print ("Enter The Second String :")
+strTwo = input()
+
+strThree = strOne + " "+ strTwo
+print("\nConcatenated String: ", strThree)
