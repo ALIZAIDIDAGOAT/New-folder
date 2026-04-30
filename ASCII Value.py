bb3e612d5ec7fb9d2a964e8c588e9a472848c339
ALIZAIDIDAGOAT <alizaidi3c@gmail.com>
2026-04-30 18:42:57 +0500

    ASCII Value

---
+++ ASCII Value.py
@@ -0,0 +1,79 @@
+x = input("Enter Value between 65-90: ")
+
+if x == "65":
+    print("A")
+
+elif x == "66":
+    print("B")
+
+elif x == "67":
+    print("C")
+
+elif x == "68":
+    print("D")
+
+elif x == "69":
+    print("E")
+
+elif x == "70":
+    print("F")
+
+elif x == "71":
+    print("G")
+
+elif x == "72":
+    print("H")
+
+elif x == "73":
+    print("I")
+
+elif x == "74":
+    print("J")
+
+elif x == "75":
+    print("K")
+
+elif x == "76":
+    print("L")
+
+elif x == "77":
+    print("M")
+
+elif x == "78":
+    print("N")
+
+elif x == "79":
+    print("O")
+
+elif x == "80":
+    print("P")
+
+elif x == "81":
+    print("Q")
+
+elif x == "82":
+    print("R")
+
+elif x == "83":
+    print("S")
+
+elif x == "84":
+    print("T")
+
+elif x == "85":
+    print("U")
+
+elif x == "86":
+    print("V")
+
+elif x == "87":
+    print("W")
+
+elif x == "88":
+    print("X")
+
+elif x == "89":
+    print("Y")
+
+elif x == "90":
+    print("Z")
