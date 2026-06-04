ea10f8dd908602e85e2f8c4eb45ac5ac82ed8ba8
ALIZAIDIDAGOAT <alizaidi3c@gmail.com>
2026-06-04 18:16:14 +0500

    Check Age

---
+++ Check Age.py
@@ -0,0 +1,5 @@
+age = int(input("How old are you?"))
+if age > 10 and age < 20:
+    print("You are allowed in class age limit between 10 and 20!")
+elif age < 10 or age > 20:
+    print("Get out.Now.")
