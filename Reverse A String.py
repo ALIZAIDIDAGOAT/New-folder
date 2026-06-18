3750bd05b0448923bf29c063feef32ab6155f1a2
ALIZAIDIDAGOAT <alizaidi3c@gmail.com>
2026-06-18 17:48:12 +0500

    Reverse A String

---
+++ Reverse A String.py
@@ -0,0 +1,10 @@
+string = input("Please enter a sensibul string: ")
+
+string2 = ('')
+
+for i in string:
+
+    string2 = i + string2
+
+    print("\nThe original string = ", string)
+    print("The reversed string = ", string2)
